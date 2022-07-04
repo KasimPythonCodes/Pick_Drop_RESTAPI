8c1d5a9261a448016d0fa56c37bc39f8a000bca4
kasim-saifi <kasim@veeaargroup.com>
2022-07-04 17:16:40 +0530

    first commit

--- distanceapi/urls.py
+++ distanceapi/urls.py
@@ -6,6 +6,7 @@ from Userform.views import*
 
 from Confirm_Booking.views import*
 from rest_framework import routers
+
 router=routers.DefaultRouter()
 
 router.register('pick_and_drop_api' , RegisterAPI, basename='pick_and_drop_api')
@@ -23,16 +24,14 @@ urlpatterns = [
     path('success' , success ,name='success'),
     
     path('check/pick_and_drop_api/api_view' , Check.as_view()  , name='check/pick_and_drop_api' ),
-    
     path('check/point/Check_Champion' ,  Check_Champion.as_view() ,name='check/point/Check_Champion'),
     path('check/point/Check_bike' ,Check_bike.as_view() ,name='check/point/Check_bike'),
-    
     path('check/pick_and_drop_api/Check_ecovan' , Check_ecovan.as_view()  , name='check/pick_and_drop_api/Check_ecovan' ),
     path('check/pick_and_drop_api/Check_Maruti_suzuki_super_carry_Trip' , Check_Maruti_suzuki_super_carry_Trip.as_view()  , name='check/pick_and_drop_api/Check_Maruti_suzuki_super_carry_Trip' ),
     path('check/pick_and_drop_api/Check_Tata_ace_7FT_Trip' , Check_Tata_ace_7FT_Trip.as_view()  , name='check/pick_and_drop_api/Check_Tata_ace_7FT_Trip' ),
     path('check/pick_and_drop_api//Check_Tata_ace_8FT_Trip' , Check_Tata_ace_8FT_Trip.as_view()  , name='check/pick_and_drop_api/Check_Tata_ace_8FT_Trip' ),
     path('check/pick_and_drop_api/Check_Blero_8FT_Trip' , Check_Blero_8FT_Trip.as_view()  , name='check/pick_and_drop_api/Check_Blero_8FT_Trip' ),
     
-
-
 ]
+
+
